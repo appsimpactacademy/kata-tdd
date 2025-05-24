4c578aee14fe2ca5eab3a69a60af06693361eda0
Ravi Singh <ravionrails@gmail.com>
2025-05-24 11:48:13 +0530

    Added a failing spec to return only number present in the string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe StringCalculator do
       result = obj.add('')
       expect(result).to eq(0)
     end
+
+    it 'returns the number as integer when string contains only one number' do 
+      obj = StringCalculator.new
+      result = obj.add('1')
+      expect(result).to eq(1)
+    end
   end
 end
