639128caeb70ed1f524b71f13ee5db98c441bb44
Ravi Singh <ravionrails@gmail.com>
2025-05-24 12:32:35 +0530

    Changed the add method to raise exception when there are negative numbers in the string

--- string_calculator.rb
+++ string_calculator.rb
@@ -9,6 +9,14 @@ class StringCalculator
     return numbers[0].to_i if numbers.size == 1
 
     numbers = numbers.map(&:to_i)
+
+    # filter out the negative number
+    negatives = numbers.select { |n| n < 0 }
+
+    # if any negative number present in the given string raise an exception
+    unless negatives.empty?
+      raise ArgumentError, "negative numbers not allowed #{negatives.join(',')}"
+    end
     numbers.sum
   end
 end
