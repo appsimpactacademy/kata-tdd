49b5131ea55a646726e59bc0b157e0bf5d1bfe67
Ravi Singh <ravionrails@gmail.com>
2025-05-24 11:54:08 +0530

    Changed the add method to get the spec passed when only one number present in the string

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,9 @@
 class StringCalculator
   def add(string)
     return if string.nil?
-
     return 0 if string.empty?
+
+    numbers = string.split(',')
+    return numbers[0].to_i if numbers.size == 1
   end
 end
